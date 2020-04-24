53c5cf1f70a97cc5c6adfe02c6c1d4d407270a85
Sonem11 <miljkovicnnd@gmail.com>
2020-04-24 19:55:38 +0200

    This is a new commit for what I originally planned to be an amendmend

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ненад\desktop\all learning staff\python\django project\my_landing_pad\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
